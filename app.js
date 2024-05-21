dcf7d54f8940dacbd2aa3b66dbd7e80f68b24162
Amlend <amlendsingh@gmail.com>
2024-05-22 01:29:11 +0530

    initial-app-setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json({ extended: false }));
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.gxjlxqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(err));
